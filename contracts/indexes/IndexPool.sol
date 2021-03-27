06cfcbdb5ba41a2644300b73fdb14f6057a2f648
Matthieu <matthieu@tessan.io>
2021-03-27 13:20:12 +0400

    add getComposition to index contract

--- contracts/indexes/IndexPool.sol
+++ contracts/indexes/IndexPool.sol
@@ -8,125 +8,145 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "hardhat/console.sol";
 import "contracts/utilities/PancakeswapUtilities.sol";
 
-
 contract IndexPool is ERC20 {
+    uint256 constant POOL_FEES = 1e16; // 1% fees
+    address private immutable _indexController;
+    IUniswapV2Router02 private _pancakeRouter;
+    IUniswapV2Factory private _pancakeFactory;
+    ERC20 private immutable _BUSD;
+    address[] _underlyingTokens;
+    uint256[] _tokenWeights;
+    uint8[] _categories;
 
-  uint256 constant POOL_FEES = 1e16; // 1% fees
-  address private immutable _indexController;
-  IUniswapV2Router02 private _pancakeRouter;
-  IUniswapV2Factory private _pancakeFactory;
-  ERC20 private immutable _BUSD;
-  address[] _underlyingTokens;
-  uint256[] _targetWeights;
-  uint8[] _categories;
+    event Mint(address indexed to, uint256 amount, uint256 cost);
+    event Burn(address indexed from, uint256 amount, uint256 paid);
+    event CompositionChanged(address[] tokens, uint256[] weights);
 
+    constructor(
+        string memory name,
+        string memory symbol,
+        address[] memory underlyingTokens,
+        uint256[] memory tokenWeights,
+        address BUSD,
+        address router,
+        address indexController,
+        uint8[] memory categories
+    ) ERC20(name, symbol) {
+        require(
+            tokenWeights.length == underlyingTokens.length,
+            "Tokens and weights don't have same sizes"
+        );
+        require(
+            underlyingTokens.length >= 2,
+            "At least 2 underlying tokens are needed"
+        );
 
-  event Mint(address indexed to, uint amount, uint cost);
-  event Burn(address indexed from, uint amount, uint paid);
-
-  constructor(
-    string memory name,
-    string memory symbol,
-    address[] memory underlyingTokens,
-    uint256[] memory targetWeights,
-    address BUSD,
-    address router,
-    address indexController,
-    uint8[] memory categories
-  ) ERC20(name, symbol) {
-    require(targetWeights.length == underlyingTokens.length, "Tokens and weights don't have same sizes");
-    require(underlyingTokens.length >= 2, "At least 2 underlying tokens are needed");
-
-    _underlyingTokens = underlyingTokens;
-    _BUSD = ERC20(BUSD);
-    _pancakeRouter = IUniswapV2Router02(router);
-    _pancakeFactory = IUniswapV2Factory(_pancakeRouter.factory());
-    _targetWeights = targetWeights;
-    _indexController = indexController;
-    _categories = categories;
-  }
-
-  function mint(uint256 BUSDIn, uint256 minAmountOut) public {
-    _BUSD.transferFrom(msg.sender, address(this), BUSDIn);
-
-    uint256 totalTokensBought = 0;
-    uint256 totalSpent = 0;
-    for (uint i = 0; i < _underlyingTokens.length; i++) {
-      (uint256 boughtAmount, uint256 spent) = PancakeswapUtilities.buyToken(
-        address(_BUSD),
-        _underlyingTokens[i],
-        address(this),
-        minAmountOut * _targetWeights[i],
-        _pancakeRouter);
-
-      totalTokensBought += boughtAmount;
-      totalSpent += spent;
+        _underlyingTokens = underlyingTokens;
+        _BUSD = ERC20(BUSD);
+        _pancakeRouter = IUniswapV2Router02(router);
+        _pancakeFactory = IUniswapV2Factory(_pancakeRouter.factory());
+        _tokenWeights = tokenWeights;
+        _indexController = indexController;
+        _categories = categories;
     }
 
-    uint256 amountOut = totalTokensBought / _sum(_targetWeights);
-    totalSpent += _collectFee(totalSpent);
+    function mint(uint256 BUSDIn, uint256 minAmountOut) public {
+        _BUSD.transferFrom(msg.sender, address(this), BUSDIn);
 
-    // refund the extra BUSD
-    _BUSD.transfer(msg.sender, BUSDIn - totalSpent);
+        uint256 totalTokensBought = 0;
+        uint256 totalSpent = 0;
+        for (uint256 i = 0; i < _underlyingTokens.length; i++) {
+            (uint256 boughtAmount, uint256 spent) =
+                PancakeswapUtilities.buyToken(
+                    address(_BUSD),
+                    _underlyingTokens[i],
+                    address(this),
+                    minAmountOut * _tokenWeights[i],
+                    _pancakeRouter
+                );
 
-    _mint(msg.sender, amountOut);
-    emit Mint(msg.sender, amountOut, totalSpent);
-  }
+            totalTokensBought += boughtAmount;
+            totalSpent += spent;
+        }
 
-  function burn(uint256 amount) public {
-    require(amount <= balanceOf(msg.sender), "Insufficient balance");
+        uint256 amountOut = totalTokensBought / _sum(_tokenWeights);
+        totalSpent += _collectFee(totalSpent);
 
-    uint256 totalTokensSold = 0;
-    uint256 amountToPayUser = 0;
+        // refund the extra BUSD
+        _BUSD.transfer(msg.sender, BUSDIn - totalSpent);
 
-    for (uint i = 0; i < _underlyingTokens.length; i++) {
-      uint256 sellAmount = (amount * _targetWeights[i]);
-      (uint256 amountOut, uint256 amountIn) = PancakeswapUtilities.sellToken(
-        _underlyingTokens[i],
-        address(_BUSD),
-        address(this),
-        sellAmount,
-        _pancakeRouter
-      );
-
-      totalTokensSold += amountIn;
-      amountToPayUser += amountOut;
+        _mint(msg.sender, amountOut);
+        emit Mint(msg.sender, amountOut, totalSpent);
     }
-    uint256 amountToBurn = totalTokensSold / _sum(_targetWeights);
-    amountToPayUser -= _collectFee(amountToPayUser);
-    _BUSD.transfer(msg.sender, amountToPayUser);
 
-    _burn(msg.sender, amountToBurn);
-    emit Burn(msg.sender, amountToBurn, amountToPayUser);
-  }
+    function burn(uint256 amount) public {
+        require(amount <= balanceOf(msg.sender), "Insufficient balance");
 
-  function getPoolPriceBUSD() public view returns (uint256) {
-    uint256 total = 0;
-    for (uint256 i = 0; i < _underlyingTokens.length; i++) {
-      total += getTokenPriceBUSD(_underlyingTokens[i]) * _targetWeights[i];
+        uint256 totalTokensSold = 0;
+        uint256 amountToPayUser = 0;
+
+        for (uint256 i = 0; i < _underlyingTokens.length; i++) {
+            uint256 sellAmount = (amount * _tokenWeights[i]);
+            (uint256 amountOut, uint256 amountIn) =
+                PancakeswapUtilities.sellToken(
+                    _underlyingTokens[i],
+                    address(_BUSD),
+                    address(this),
+                    sellAmount,
+                    _pancakeRouter
+                );
+
+            totalTokensSold += amountIn;
+            amountToPayUser += amountOut;
+        }
+        uint256 amountToBurn = totalTokensSold / _sum(_tokenWeights);
+        amountToPayUser -= _collectFee(amountToPayUser);
+        _BUSD.transfer(msg.sender, amountToPayUser);
+
+        _burn(msg.sender, amountToBurn);
+        emit Burn(msg.sender, amountToBurn, amountToPayUser);
     }
-    return total;
-  }
 
-  function getTokenPriceBUSD(address token) public view returns (uint256) {
-    address pairBUSDAddr = _pancakeFactory.getPair(address(_BUSD), token);
-    require(pairBUSDAddr != address(0), "Cannot find pair BUSD-token");
-    IUniswapV2Pair pairBUSD = IUniswapV2Pair(pairBUSDAddr);
-    (uint reserveBUSD, uint reserveToken) = PancakeswapUtilities.getReservesOrdered(pairBUSD, address(_BUSD), token);
-    return _pancakeRouter.quote(1e18, reserveToken, reserveBUSD);
-  }
-
-  function _sum(uint256[] memory items) private pure returns (uint256) {
-    uint256 total = 0;
-    for (uint i = 0; i < items.length; i++) {
-      total += items[i];
+    function getPoolPriceBUSD() public view returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < _underlyingTokens.length; i++) {
+            total += getTokenPriceBUSD(_underlyingTokens[i]) * _tokenWeights[i];
+        }
+        return total;
     }
-    return total;
-  }
 
-  function _collectFee(uint256 amount) private returns(uint256) {
-    uint256 fee = (amount * POOL_FEES) / 1e18;
-    _BUSD.transfer(_indexController, fee);
-    return fee;
-  }
+    function getTokenPriceBUSD(address token) public view returns (uint256) {
+        address pairBUSDAddr = _pancakeFactory.getPair(address(_BUSD), token);
+        require(pairBUSDAddr != address(0), "Cannot find pair BUSD-token");
+        IUniswapV2Pair pairBUSD = IUniswapV2Pair(pairBUSDAddr);
+        (uint256 reserveBUSD, uint256 reserveToken) =
+            PancakeswapUtilities.getReservesOrdered(
+                pairBUSD,
+                address(_BUSD),
+                token
+            );
+        return _pancakeRouter.quote(1e18, reserveToken, reserveBUSD);
+    }
+
+    function getComposition()
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        return (_underlyingTokens, _tokenWeights);
+    }
+
+    function _sum(uint256[] memory items) private pure returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < items.length; i++) {
+            total += items[i];
+        }
+        return total;
+    }
+
+    function _collectFee(uint256 amount) private returns (uint256) {
+        uint256 fee = (amount * POOL_FEES) / 1e18;
+        _BUSD.transfer(_indexController, fee);
+        return fee;
+    }
 }

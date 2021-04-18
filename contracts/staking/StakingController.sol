d5e523c74d4b7eb201cecda21c729d32718576d7
Matthieu <mfron.42@gmail.com>
2021-04-18 19:53:25 +0400

    add create stacking only by owner

--- contracts/staking/StakingController.sol
+++ contracts/staking/StakingController.sol
@@ -3,18 +3,25 @@ import "contracts/staking/StakingPool.sol";
 
 contract StakingPoolController {
     address constant UNISWAP_ROUTER = 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F;
+    address _owner;
     SLEVToken _SLEV;
     IUniswapV2Router02 _router;
 
-    constructor() {
+    constructor(address owner) {
+        _owner = owner;
         _SLEV = new SLEVToken(address(this), 0);
         _router = IUniswapV2Router02(UNISWAP_ROUTER);
+    }
+
+    modifier ownerOnly {
+        require(msg.sender == _owner, "STACKING_CONTROLLER: NOT_THE_OWNER");
+        _;
     }
 
     function deployStackingPool(
         address stakeToken, 
         address[] memory rewardTokens,
-        uint256[] memory multiplier) external  {
+        uint256[] memory multiplier) external ownerOnly {
         StakingPool pool = new StakingPool(address(_SLEV), stakeToken, rewardTokens, multiplier, _router);
         _SLEV.addMinter(address(pool));
     }
